f2f8ce932585e7fcdfe5cf895ff2190c4a52452d
Espla <37451532+alejandroespla@users.noreply.github.com>
2025-06-13 18:38:49 +0000

    icono

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -51,17 +51,17 @@ const Home = () => {
   <p className="p-5 notask">No task defined</p>
 ) : (
   list.map((item, index) => (
-    <div key={index} className="d-flex toDo-item text-start px-3 py-2">
+    <div key={index} className="d-flex toDo-item text-start px-3 py-2 ">
       <div className="task d-flex align-items-center">
         <span className="border-0">{item.listItem}</span>
       </div>
       <div>
-        <img 
-          onClick={() => deleteToDo(index)}
-          className="delete-icon"  
-          src="../src/img/x.svg" 
-          alt="delete" 
-        />
+        <svg 
+        onClick={() => deleteToDo(index)}
+        xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x delete-icon" viewBox="0 0 16 16">
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+        </svg>    
+      
       </div>
     </div>
   ))
